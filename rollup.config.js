6246ecd399c42aa3c1f6f3f5c357f49138792aba
nujarum <84607911+nujarum@users.noreply.github.com>
2021-10-02 16:01:57 +0900

    chore: update `rollup.config.js`

--- rollup.config.js
+++ rollup.config.js
@@ -1,1 +1,74 @@
-export default {};
+import { builtinModules } from 'module';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
+import typescript from 'rollup-plugin-ts';
+
+const external = Object.freeze([
+    ...builtinModules,
+    /([/\\])node_modules\1/,
+    /^#.+/,
+]);
+const outputDeclaration = Object.freeze({
+    declaration: true,
+    declarationDir: './types',
+    declarationMap: true,
+});
+const terserOptions = Object.freeze({
+    compress: false,
+    // format: { beautify: true },  // for debug
+    ecma: new Date().getFullYear(),
+});
+
+export default [
+    {
+        input: 'src/main.ts',
+        external,
+        output: {
+            file: 'dist/main.mjs',
+            format: 'es',
+            preferConst: true,
+            sourcemap: true,
+        },
+        plugins: [
+            nodeResolve(),
+            typescript({
+                tsconfig: cfg => ({
+                    ...cfg,
+                    ...outputDeclaration,
+                }),
+            }),
+            terser(terserOptions),
+        ],
+    },
+    {
+        input: 'src/worker.ts',
+        external,
+        output: {
+            file: 'dist/worker.mjs',
+            format: 'es',
+            preferConst: true,
+            sourcemap: true,
+        },
+        plugins: [
+            nodeResolve(),
+            typescript(),
+            terser(terserOptions),
+        ],
+    },
+    {
+        input: 'src/cli.ts',
+        external,
+        output: {
+            banner: '#!/usr/bin/env node',
+            file: 'bin/cli.mjs',
+            format: 'es',
+            preferConst: true,
+            sourcemap: true,
+        },
+        plugins: [
+            nodeResolve(),
+            typescript(),
+            terser(terserOptions),
+        ],
+    },
+];
